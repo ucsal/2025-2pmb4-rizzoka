ebdc8974b52c3fc5b4b13b12908441c5775176c6
RizzoKa <kauarizzo06@gmail.com>
2025-09-15 22:30:12 -0300

    add: adicioanndo menu e terminando a aplicação

--- src/main/java/br/com/mariojp/figureeditor/App.java
+++ src/main/java/br/com/mariojp/figureeditor/App.java
@@ -1,7 +1,17 @@
 package br.com.mariojp.figureeditor;
 
-import javax.swing.*;
-import java.awt.*;
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.WindowConstants;
+
+import abstracts.CircleFactory;
+import abstracts.RectangleFactory;
 
 public class App {
     public static void main(String[] args) {
@@ -13,7 +23,25 @@ public class App {
             JFrame frame = new JFrame("Figure Editor — Clique para inserir figuras");
             frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-            DrawingPanel panel = new DrawingPanel();
+            DrawingPanel panel = new DrawingPanel(new CircleFactory());
+            JMenuBar menuBar = new JMenuBar(); 
+            JMenu menu = new JMenu("Desenhos"); //iniciando um menu
+            
+            JMenuItem circleItem = new JMenuItem("Círculo");
+            circleItem.addActionListener(e -> panel.setShapeFactory(new CircleFactory()));
+
+            JMenuItem rectItem = new JMenuItem("Retângulo");
+            rectItem.addActionListener(e -> panel.setShapeFactory(new RectangleFactory())); 
+           
+            JMenuItem clearItem = new JMenuItem("Limpar Tudo");
+            clearItem.addActionListener(e -> panel.clear());
+            
+            menu.add(circleItem);
+            menu.add(rectItem);
+            menu.addSeparator();
+            menu.add(clearItem);
+            menuBar.add(menu);
+            frame.setJMenuBar(menuBar);
 
             frame.setLayout(new BorderLayout());
             frame.add(panel, BorderLayout.CENTER);
